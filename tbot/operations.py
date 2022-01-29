8c61b2512f83a7f065d878899c95ff3eabf8cf85
Viktor Ermolov <viktor@ermolov.dev>
2022-01-29 05:09:18 -0500

    cleaning

--- tbot/operations.py
+++ tbot/operations.py
@@ -55,11 +55,6 @@ def update_product(id):
         product.title, product.price, product.availability = get_amazone_data(
             product.url_field)
         product.save()
-        # print(f"Product updated: {product.title}")
-        # BOT.send_message(chat_id=CHAT_ID, text=f"{product.title} updated")
-    # else:
-    #     print(f"Product doesn't exist: {id}")
-        # BOT.send_message(chat_id=CHAT_ID, text=f"{id} doesn't exist")
 
 
 def remove_product(id):
@@ -70,10 +65,8 @@ def remove_product(id):
     if bool(exists):
         product = Product.get(Product.id == id)
         product.delete_instance()
-        # print(f"Product deleted: {product.title}")
         return f"{product.title} удален"
     else:
-        # print(f"Product doesn't exist: {id}")
         return f"{id} такого товара нет"
 
 
@@ -97,31 +90,3 @@ def check_update():
         else:
             BOT.send_message(chat_id=CHAT_ID, text="Nothing new")
         time.sleep(15)
-
-        # if product.availability != availability:
-        #     update_product(product.id)
-        #     BOT.send_message(chat_id=CHAT_ID, text=f"Availability {product.title} updated to {availability}")  # noqa
-        # elif product.price != price:
-        #     BOT.send_message(chat_id=CHAT_ID, text=f"Price {product.title} updated from {product.price} to {price}")  # noqa
-        #     update_product(product.id)
-        # elif product.title != title:
-        #     update_product(product.id)
-        #     BOT.send_message(chat_id=CHAT_ID, text=f"Title updated to {title}")
-        # else:
-        #     BOT.send_message(chat_id=CHAT_ID, text="Nothing new")
-        # time.sleep(30)
-
-
-if __name__ == "__main__":
-    # new_product = get_amazone_data(URL)
-    get_all_products()
-    # check_update()
-    # pampers = Product(title=new_product[0], price=new_product[1], availability=new_product[2], url_field=URL)  # noqa
-    # pampers.save()
-    # create_product("https://www.amazon.com/Pampers-Training-Underwear-5t-6t-Count/dp/B01M2CZBCD/ref=sr_1_3?crid=KSTW33I9DL74&dchild=1&keywords=pampers+easy+ups+5t-6t&qid=1626787064&sprefix=pampers+ea%2Caps%2C195&sr=8-3")
-    # for produst in Product.select():
-    #     print(produst.title, '\n', produst.availability,
-    #         '\n', produst.price, '\n', produst.url_field)
-    #     print(type(produst.id))
-    # BOT.send_message(chat_id=CHAT_ID, text=produst.title)
-    # BOT.send_message(chat_id=CHAT_ID, text=produst.availability)
